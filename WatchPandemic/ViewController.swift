937429ecd6c502a9f5396572a3f971b3e8b24121
Sirat Samyoun <ss8hf@virginia.edu>
2020-08-03 18:10:01 -0400

    empty xcode

---
+++ WatchPandemic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchPandemic
+//
+//  Created by Sirat Samyoun on 8/3/20.
+//  Copyright © 2020 Sirat Samyoun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
